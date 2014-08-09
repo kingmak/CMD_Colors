f552f1a51ce8f5a29b7f7ea6ccacf152771912d0
King Mak <kingmak@hotmail.com>
2014-08-09 12:28:17 -0500

    Create cmd_colors.py

---
+++ cmd_colors.py
@@ -0,0 +1,15 @@
+# bad variable names, yea i know
+from os import system
+
+x = ['0', '1', '2', '3' ,'4', '5', '6', '7']
+y = ['8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+while True:
+    for _ in x:
+        for __ in y:
+            color = _ + __
+            system('color ' + color)
+            raw_input('Color is: ' + color + ' ')
+            system('cls')
+            
+raw_input('\n\nDone\n')
